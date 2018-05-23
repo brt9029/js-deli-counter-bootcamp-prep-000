dbfbae0866291dc17c5c11ac865a84ca44fe561c
Bryant Torres <brt9029@gmail.com>
2018-05-23 19:26:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length === 0){
     return "The line is currently empty."
   } else { while (katzDeliLine.length < i){
-    `The line is curretnly: ` + `${katzDeliLine[i+1]}. ` + `${katzDeliLine[i]}, `
+    return `The line is curretnly: ` + `${katzDeliLine[i+1]}. ` + `${katzDeliLine[i]}, `
     i ++
     }
   }
