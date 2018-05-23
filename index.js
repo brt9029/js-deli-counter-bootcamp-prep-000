75f043f7dce67e71b1d8ecebbdb332c5892f8460
Bryant Torres <brt9029@gmail.com>
2018-05-23 19:37:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
   var i = 0
-  var linePlace = `${katzDeliLine[i+1]}. `
+  var linePlace = `${[i+1]}. `
   var personName = `${katzDeliLine[i]}`
   if (katzDeliLine.length === 0){
     return "The line is currently empty."
