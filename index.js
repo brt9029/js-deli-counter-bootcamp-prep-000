2a2318d7cb4aaa58f879da04622f821320132248
Bryant Torres <brt9029@gmail.com>
2018-05-23 19:51:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,16 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
+  var i = 0
   var line = "The line is currently: "
   if (katzDeliLine.length === 0){
     return "The line is currently empty."
   } else {
-    for (let i = 0; i < katzDeliLine.length; i++){
-      line += `${[i+1]}. ${katzDeliLine[i]}, `
-    }
+    do {
+      line += `${[i+1]}. ${katzDeliLine[i]}`
+      i ++
+      line += ", "
+    } while (i < katzDeliLine.length)
   }
   return line
 }
