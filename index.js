46c85e1f4c0a4fced2e4bd3e694f136a075382bd
Bryant Torres <brt9029@gmail.com>
2018-05-23 18:12:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ function takeANumber(katzDeliLine, name){
 }
 
 function nowServing(katzDeliLine){
+  if (katzDeliLine.length == 0){
+    return "The line is currently empty."
+  }
   return `Currently serving ${katzDeli[0]}.`
     katzDeli.shift()
 }
