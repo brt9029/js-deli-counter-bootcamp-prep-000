d9bacc135641cffff7f28fe4384ba51864f8fe6f
Bryant Torres <brt9029@gmail.com>
2018-05-23 18:06:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = []
 
 function takeANumber(line, katzDeliLine, newPerson){
   katzDeliLine.push(newPerson)
-  return `Welcome, ${newPerson}. You are number ${line.length} in line.`
+  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(){
